7bcc7d01b3e8b9101a1f23c70d2826416309da84
amin3696 <a11shahimian@gmail.com>
2019-03-14 12:06:13 +0100

    make3WebApps

---
+++ Week04Web/Exercise03/Duchman/src/main/java/at/nacs/duchman/DuchmanApplication.java
@@ -0,0 +1,13 @@
+package at.nacs.duchman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuchmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuchmanApplication.class, args);
+    }
+
+}
